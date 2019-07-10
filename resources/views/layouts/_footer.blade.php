df7cff6cbf9e5439a814b6a95bd29a15eb0c823c
sv <cryvelge@foxmail.com>
2019-07-10 16:44:02 +0800

    页面基础结构

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+    <div class="container">
+        <p class="float-left">
+            由 <a href="#" target="_blank">倒立</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+        <p class="float-right">
+            <a href="mailto:cryvelge@foxmail.com">联系我们</a>
+        </p>
+    </div>
+</footer>
