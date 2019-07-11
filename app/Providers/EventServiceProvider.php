86666f4ffa5ca89768214691a8e6c2c93d157dca
sv <cryvelge@foxmail.com>
2019-07-11 14:43:11 +0800

    使用事件实现认证后的提醒消息

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Auth\Events\Verified;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
@@ -18,6 +19,10 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+
+        Verified::class => [
+            \App\Listeners\EmailVerified::class
+        ]
     ];
 
     /**
