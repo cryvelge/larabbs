24e41e9a69a96be943203e26ba934795c5949157
vagrant <vagrant@homestead>
2019-07-11 09:32:56 +0000

    用户个人资料修改以及展示

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Requests\UserRequest;
+
 
 class UsersController extends Controller
 {
@@ -11,5 +13,16 @@ class UsersController extends Controller
     {
         return view('users.show', compact('user'));
     }
-    //
+
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    public function update(UserRequest $request, User $user)
+    {
+        $user->update($request->all());
+        return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功');
+
+    }
 }
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -8,10 +8,10 @@
                      alt="{{ $user->name }}">
                 <div class="card-body">
                     <h5><strong>个人简介</strong></h5>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+                    <p>{{$user->introduction }} </p>
                     <hr>
                     <h5><strong>注册于</strong></h5>
-                    <p>January 01 1901</p>
+                    <p>{{$user->created_at->diffForHumans()}}</p>
                 </div>
             </div>
         </div>
