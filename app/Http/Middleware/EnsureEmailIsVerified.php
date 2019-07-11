c33d5a786125bb36b2e9147332137722158cb28c
sv <cryvelge@foxmail.com>
2019-07-11 10:39:32 +0800

    邮箱认证

---
+++ app/Http/Middleware/EnsureEmailIsVerified.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class EnsureEmailIsVerified
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if ($request->user() &&
+            ! $request->user()->hasVerifiedEmail() &&
+            !$request->is('email/*', 'logout')) {
+            return $request->expectsJson()
+                ? abort(403, 'Your email address is not verified')
+                : redirect()->route('verification.notice');
+        }
+
+        return $next($request);
+    }
+}
