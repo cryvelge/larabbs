762b2b99c7cbd8242e7393ef0cba180d23e64921
vagrant <vagrant@homestead>
2019-07-12 07:38:48 +0000

    裁剪过大头像

--- app/Handlers/ImageUploadHandler.php
+++ app/Handlers/ImageUploadHandler.php
@@ -2,12 +2,14 @@
 
 namespace App\Handlers;
 
+use Image;
+
 class ImageUploadHandler
 {
     //允许的图片格式
     protected $allowed_ext = ['png', 'jpg', 'gif', 'jpeg'];
 
-    public function save($file, $folder, $file_prefix)
+    public function save($file, $folder, $file_prefix, $max_width = false)
     {
         //存储的文件夹规则，例如：uploads/images/avatars/201709/21/
         // tips:切割文件夹能让查找效率更高
@@ -29,9 +31,32 @@ class ImageUploadHandler
         //将图片移动到存储路径中
         $file->move($upload_path, $file_name);
 
+        if ($max_width && $extension != 'gif') {
+
+            $this->reduceSize($upload_path.'/'.$file_name, $max_width);
+        }
+
         return [
             'path' => config('app.url')."/$folder_name/$file_name"
         ];
+    }
+
+    public function reduceSize($file_path, $max_width)
+    {
+        //实例化，参数是图片的物理磁盘位置
+        $image = Image::make($file_path);
+
+        //进行调整大小的操作
+        $image->resize($max_width, null, function ($constraint) {
+            //设定宽度是 $max_width ,高度等比例缩放
+            $constraint->aspectRatio();
+
+            //防止裁图时图片尺寸变大
+            $constraint->upsize();
+
+        });
+
+        $image->save();
     }
 
 }
